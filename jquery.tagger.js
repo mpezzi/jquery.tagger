24011122094883a767dab237cde891aca86517ec
Mike Pezzi <mike@thespiral.ca>
2010-03-10 10:44:31 -0500

    Coded initial structure for text component

--- jquery.tagger.js
+++ jquery.tagger.js
@@ -13,26 +13,89 @@
 $.fn.tagger = function(options) {
   return this.each(function(){
     var self = $(this),
-        opts = $.extend({}, $.fn.tagger.defaults, options);
+        opts = $.extend({}, $.fn.tagger.defaults, options),
+        tagger = $.fn.tagger.component[self.context.type],
+        container = $('<ul class="tagger"></ul>').insertAfter(self);
+    
+    tagger.element = self;
+    tagger.init(opts);
+    
+    $.each(tagger.list(), function(i, t){
+      $('<li>').text(t).css('cursor', 'pointer').click(function(){
+        tagger.has($(this)) ?
+          tagger.remove($(this)) : tagger.add($(this));
+        
+        // Cancel event propagations.
+        return false;
+      }).appendTo(container);
+    });
     
     self.disableTextSelect();
   });
 };
 
 $.fn.tagger.defaults = {
-  
+  separator: ', ',
+  active: 'selected'
 };
 
+$.fn.tagger.component = [];
+
 // Declare form type components.
-$.fn.tagger.textfield = {
-  element: function() {
-    return $('input[type=text]', this.container);
+$.fn.tagger.component['text'] = {
+  init: function(opts) {
+    this.opts = opts;
+  },
+  has: function(tag) {
+    var has = false;
+    
+    $.each(this.list(), function(i, t){
+      if ( t == tag.text() )
+        has = true;
+    });
+    
+    return has;
+  },
+  add: function(tag) {
+    this.element.val() ?
+      this.element.val(this.element.val() + this.opts.separator + tag.text()) :
+      this.element.val(tag.text());
+    
+    tag.addClass(this.opts.active);
+  },
+  remove: function(tag) {
+    var self = this;
+    
+    $.each(self.list(), function(i, t){
+      if ( t == tag.text() ) {
+        list = self.list();
+        list.splice(i, 1);
+        self.element.val(list.join(self.opts.separator));
+      }
+    });
+    
+    tag.removeClass(this.opts.active);
+  },
+  list: function() {
+    return this.element.val().split(this.opts.separator);
   }
 };
 
-$.fn.tagger.select = {
-  element: function() {
-    return $('select', this.container);
+$.fn.tagger.component['select-multiple'] = {
+  init: function(opts) {
+    
+  },
+  has: function(tag) {
+    
+  },
+  add: function(tag) {
+    
+  },
+  remove: function(tag) {
+    
+  },
+  list: function() {
+    
   }
 };
 
@@ -55,4 +118,4 @@ function log(message) {
     window.console.log(message);
 }
 
-});
+})(jQuery);
